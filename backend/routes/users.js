78a5b9a76c16368d497fe1a0e64f88dc5036eb5f
VANIJAIN007 <vani.jain_cs19@gla.ac.in>
2022-04-21 11:21:56 -0700

    user file

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const router=require("express").Router();
-const Pin = require("../models/Pin");
-
-router.post("/",(req,res)=>{
-    const newPin=new Pin(req.body);
-    try{
-        const savedPin=await newPin.save();
-    }
-});
