e78ad6449c3d441b08da5c7d401a3d356c37d2f5
VANIJAIN007 <vani.jain_cs19@gla.ac.in>
2022-05-25 04:11:51 +0530

    updated Appjs file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
-
-
+// import {useState} from 'react';
+// import ReactMapGl from 'react-map-gl';
+import * as React from 'react';
+import Map from 'react-map-gl';
 function App() {
+  // const[viewport,setviewport]=useState({
+  //   longitude:-122.436,
+  //   latitde: 37.7577,
+  //   width: 400,
+  //   height:400,
+  //   zoom:8
+
+  // });
+  // return (
+  //   <div className="App">
+  //     <h1>hello</h1>
+  //     <ReactMapGl
+  //     {...viewport}
+  //     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+  //     onViewportChange={nextViewport => setviewport(nextViewport)}
+  //     />
+  //   </div>
+  //);
   return (
-    <div className="App">
-      hello
-    </div>
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
+  
 }
 
 export default App;
